819d774c0e703d7ad35b2f89769db9a3ec602981
Stephen Margheim <stephen.margheim@gmail.com>
2024-04-07 15:41:43 +0200

    Use Phlex-style layout handling

--- app/views/layouts/application_layout.rb
+++ app/views/layouts/application_layout.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationLayout < ApplicationView
+class ApplicationLayout < ApplicationComponent
   include Phlex::Rails::Layout
   include Phlex::Rails::Helpers::ContentFor
   include Phlex::Rails::Helpers::LinkTo
@@ -16,7 +16,7 @@ class ApplicationLayout < ApplicationView
         meta(name: "apple-mobile-web-app-capable", content: "yes")
         csrf_meta_tags
         csp_meta_tag
-        plain yield :head
+        yield :head
         link(rel: "manifest", href: "/manifest.json")
         link(rel: "icon", href: "/icon.png", type: "image/png")
         link(rel: "icon", href: "/icon.svg", type: "image/svg+xml")
@@ -73,7 +73,7 @@ class ApplicationLayout < ApplicationView
         main(
           class:
             "border-2 border-black bg-white px-4 py-8 relative mb-4 before:absolute before:inset-0 before:-z-10 before:transform before:translate-x-2 before:translate-y-2 before:bg-[radial-gradient(circle_at_center,#000_.05rem,transparent_0),radial-gradient(circle_at_center,#000_.05rem,transparent_0)] before:[background-size:.2rem_.2rem] before:[background-position:0_0,.1rem_.1rem] before:[background-color:initial]"
-        ) { plain yield }
+        ) { yield }
         footer(class: "mt-auto text-sm text-center") do
           p(class: "py-4") do
             plain " Made with &heartsuit; by "
